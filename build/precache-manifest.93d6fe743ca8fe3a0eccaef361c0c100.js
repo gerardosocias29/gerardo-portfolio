2ef3595b901c1d1a1df4646ce40968f944c3fea0
Gerardo <g.socias29@gmail.com>
2023-07-11 20:09:22 +0800

    build updates

---
+++ build/precache-manifest.93d6fe743ca8fe3a0eccaef361c0c100.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f39848f38c00e7714e3",
+    "url": "/gerardo-portfolio/static/css/main.3a659a6b.chunk.css"
+  },
+  {
+    "revision": "1f39848f38c00e7714e3",
+    "url": "/gerardo-portfolio/static/js/main.1f39848f.chunk.js"
+  },
+  {
+    "revision": "063a5958033486a6cc87",
+    "url": "/gerardo-portfolio/static/js/1.063a5958.chunk.js"
+  },
+  {
+    "revision": "2f167904c0ce8bb25b86",
+    "url": "/gerardo-portfolio/static/js/runtime~main.2f167904.js"
+  },
+  {
+    "revision": "b47532213874f6cb5e7cad9ce8f06c5b",
+    "url": "/gerardo-portfolio/index.html"
+  }
+];
